bec6c6900bb2ffb313d128046784b8a2c83285d4
Moaaz Hail <hailmoaaz@gmail.com>
2025-12-21 20:33:31 +0200

    feat: Header Setup And Responsiveness

---
+++ src/lib/constants/header.constant.ts
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    label: "home",
+    link: "/",
+  },
+  {
+    label: "about",
+    link: "/about",
+  },
+  {
+    label: "classes",
+    link: "/classes",
+  },
+  {
+    label: "healthy",
+    link: "/healthy",
+  },
+];
