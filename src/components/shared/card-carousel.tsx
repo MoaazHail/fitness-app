fd4b8ea991932c7608c844079cf4146001eb5fe0
Moaaz Hail <hailmoaaz@gmail.com>
2026-01-08 14:46:44 +0200

    feat: Create A Card Carousel

---
+++ src/components/shared/card-carousel.tsx
@@ -0,0 +1,32 @@
+import Card from "@/components/shared/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import type { CardModel } from "@/lib/types/card";
+import type { EmblaOptionsType } from "embla-carousel";
+
+type CardCarouselProps = {
+  products?: CardModel[];
+};
+export default function CardCarousel({ products }: CardCarouselProps) {
+  // Options
+  const OPTIONS: EmblaOptionsType = {
+    loop: true,
+    slidesToScroll: 1,
+    align: "start",
+  };
+
+  return (
+    <Carousel opts={OPTIONS} className=" uppercase font-bold mb-20">
+      <CarouselContent className="">
+        {products?.map((item, index) => (
+          <CarouselItem className="basis-auto mr-10" key={index}>
+            <Card item={item} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+}
